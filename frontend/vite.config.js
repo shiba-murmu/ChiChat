9855afafcd3ed8cceb3c98e3e118ca05e2b6cd2f
Shiba Murmu <shibamurmu001@gmail.com>
2025-06-29 22:07:59 +0530

    one more file is added to the directory and

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+   darkMode: "class",
+   content: ['./src/**/*.{html,js,jsx,ts,tsx}'], // adjust based on our project
+   theme: {
+    extend: {},
+  },
   plugins: [react(), tailwindcss()],
 });
