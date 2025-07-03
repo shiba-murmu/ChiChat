900310033a98750c3b68d2a65e212f620a4ff0a2
Shiba Murmu <shibamurmu001@gmail.com>
2025-07-03 11:14:41 +0530

    removed spaces

--- frontend/src/pages/contents/Chat.jsx
+++ frontend/src/pages/contents/Chat.jsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
 import TripleDot from '../../components/triple/TripleDot'
-
-
-
 
 function SpecificUserCard() {
     return (
@@ -37,6 +33,7 @@ function SpecificUserCard() {
         </>
     )
 }
+
 function Chat() {
     return (
         <>
