02cbd167ed3dde5c925b101fa3eb3d0ddb861573
Shiba Murmu <shibamurmu001@gmail.com>
2025-07-03 09:08:06 +0530

    message component UI updated.

--- frontend/src/pages/user/Messages.jsx
+++ frontend/src/pages/user/Messages.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
 function Messages() {
-  return (
-    <div>Messages</div>
-  )
+    return (
+        <>
+            <div className='min-h-screen w-full max-w-md mx-auto bg-white shadow-lg rounded-lg overflow-hidden'>
+                Messages Page
+                <div className='p-4'>
+                    <p className='text-gray-600'>This is where your messages will be displayed.</p>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Messages
