a3818e2abbb9f5692bea9a70787da6f835129cf3
Shiba Murmu <shibamurmu001@gmail.com>
2025-07-03 12:32:44 +0530

    Notifications header added and backbutton for to navigate to feeds page.

--- frontend/src/pages/contents/Notifications.jsx
+++ frontend/src/pages/contents/Notifications.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import LeftBack from '../../components/back/LeftBack'
+function NotificationsHeader() {
+    return(
+        <>
+            <div>
+                <div className='flex justify-between items-center p-2 border-b border-gray-300'>
+                    <div className='flex items-center gap-4 p-1'>
+                        <div>
+                            <div className='flex items-center cursor-pointer'>
+                                <Link to={'/feeds'}>
+                                    <LeftBack />
+                                </Link>
+                            </div>
+                        </div>
+                        <div className='flex items-center cursor-pointer'>
+                            <span className='font-semibold text-lg'>Notifications</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
 
 function Notifications() {
   return (
    <>
         <div className='min-h-screen w-full max-w-md mx-auto bg-white shadow-lg rounded-lg overflow-hidden'>
+        <NotificationsHeader />
             <div className='flex justify-center items-center h-full'>No Notifications !!</div>
         </div>
    </>
