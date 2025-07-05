50b1596e54530b8eb9904423b801fba9b2cf188d
Shiba Murmu <shibamurmu001@gmail.com>
2025-07-05 21:38:14 +0530

    removed one line code .

--- backend/user/views.py
+++ backend/user/views.py
@@ -3,12 +3,10 @@ from django.http import HttpResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.permissions import AllowAny
 
 # Create your views here.
 
 class RegisterUser(APIView): 
-    permission_classes = [AllowAny] 
     
     def post(self, request):
         # Dummy logic : return a fixed success message
